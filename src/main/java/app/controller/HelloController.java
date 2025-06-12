237745205bb03bb103a10964c6f638a1d5f11726
Alex Balak <alex.balak@outlook.com>
2025-06-12 15:01:27 +0200

    Simple controller working.

---
+++ src/main/java/app/controller/HelloController.java
@@ -0,0 +1,14 @@
+package app.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping ("/")
+    String home() {
+        return "Home";
+    }
+}
