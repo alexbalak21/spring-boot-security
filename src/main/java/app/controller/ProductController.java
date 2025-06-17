766c529b17c6748641ce9a4a7cfca09fce8b0d40
Alex Balak <alex.balak@outlook.com>
2025-06-17 23:28:33 +0200

    Working visualizing Products

--- src/main/java/app/controller/ProductController.java
+++ src/main/java/app/controller/ProductController.java
@@ -1,9 +1,11 @@
 package app.controller;
 
+import app.model.Product;
 import app.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,8 @@ public class ProductController {
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
+    @PostMapping
+    public ResponseEntity<?> saveProduct(Product product) {
+        return ResponseEntity.ok(productService.saveProduct(product));
+    }
 }
