1fc04714f3155030b8e364db777f4e6c7362014f
Alex Balak <alex.balak@outlook.com>
2025-06-17 23:15:01 +0200

    ProductController created

---
+++ src/main/java/app/controller/ProductController.java
@@ -0,0 +1,22 @@
+package app.controller;
+
+import app.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+}
