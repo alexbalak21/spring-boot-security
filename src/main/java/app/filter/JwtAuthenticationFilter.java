bd8375737bd5214e884c26012a94fed0e6e35267
Alex Balak <alex.balak@outlook.com>
2025-06-18 21:36:35 +0200

    Jwt token authentication not working.

--- src/main/java/app/filter/JwtAuthenticationFilter.java
+++ src/main/java/app/filter/JwtAuthenticationFilter.java
@@ -54,9 +54,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
-            filterChain.doFilter(request, response);
         }
-
-
+        filterChain.doFilter(request, response);
     }
 }
