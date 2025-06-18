9df60709042b47e1b1df2de118387c19f239bd47
Alex Balak <alex.balak@outlook.com>
2025-06-18 11:32:43 +0200

    Created User Model

---
+++ src/main/java/app/model/Role.java
@@ -0,0 +1,8 @@
+package app.model;
+
+
+
+public enum Role {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/app/model/User.java
@@ -0,0 +1,48 @@
+package app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max=50, message = "Full name must be between 3 and 50 characters")
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max=50, message = "Username must be between 3 and 50 characters")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 3 characters")
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+
+    public User(String fullName, String username, String password, Role role) {
+        this.fullName = fullName;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+}
